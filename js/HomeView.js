a0ab8f8bdb57caea8372c0d4a5099f62e1031728
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-03-29 12:05:51 -0300

    cordova

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,32 +1,25 @@
-var HomeView = function(store) {
-	
- 	this.initialize = function() {
-        // Define a div wrapper for the view. The div wrapper is used to attach events.
-        this.el = $('<div/>');
-        this.el.on('keyup', '.search-key', this.findByName);
+var HomeView = function (service) {
+
+    var employeeListView;
+
+    this.initialize = function() {
+        this.$el = $('<div/>');
+        this.$el.on('keyup', '.search-key', this.findByName);
+        employeeListView = new EmployeeListView();
+        this.render();
     };
 
- 	this.render = function() {
-	    this.el.html(HomeView.template());
-	    return this;
-	};
-
+    this.render = function() {
+        this.$el.html(this.template());
+        $('.content', this.$el).html(employeeListView.$el);
+        return this;
+    };
 
     this.findByName = function() {
-	    store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(HomeView.liTemplate(employees));
-	        if (self.iscroll) {
-	            console.log('Refresh iScroll');
-	            self.iscroll.refresh();
-	        } else {
-	            console.log('New iScroll');
-	            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
-	        }
-	    });
-	};
+        service.findByName($('.search-key').val()).done(function(employees) {
+            employeeListView.setEmployees(employees);
+        });
+    };
+
     this.initialize();
-
 }
-
-HomeView.template = Handlebars.compile($("#home-tpl").html());
-HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
