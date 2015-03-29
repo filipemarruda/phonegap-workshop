fce212464ba737b35f2a934b6dc313d3f76b42ce
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-03-29 18:21:41 -0300

    camera

--- platforms/android/assets/www/js/EmployeeView.js
+++ platforms/android/assets/www/js/EmployeeView.js
@@ -31,7 +31,7 @@ var EmployeeView = function(employee) {
 	  }
 	  var options =   {   quality: 50,
 	                      destinationType: Camera.DestinationType.DATA_URL,
-	                      sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+	                      sourceType: Camera.PictureSourceType.CAMERA,
 	                      encodingType: 0     // 0=JPG 1=PNG
 	                  };
 
