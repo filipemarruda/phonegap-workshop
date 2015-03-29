11203cb9592f3d7a55b297d943c2f94d6fc11bc9
unknown <F28870B@ABRBE1LT0081630.fiatauto.adfa.local>
2015-03-29 18:01:50 -0300

    camera feature

--- platforms/android/assets/www/js/EmployeeView.js
+++ platforms/android/assets/www/js/EmployeeView.js
@@ -3,6 +3,7 @@ var EmployeeView = function(employee) {
     this.initialize = function() {
         this.$el = $('<div/>');
         this.$el.on('click', '.add-location-btn', this.addLocation);
+        this.$el.on('click', '.change-pic-btn', this.changePicture);
     };
 
     this.render = function() {
@@ -16,9 +17,33 @@ var EmployeeView = function(employee) {
 	      function(position) {
 	          alert(position.coords.latitude + ',' + position.coords.longitude);
 	      },
-	      function() {
-	          alert('Error getting location');
+	      function(error) {
+	          alert('Error getting location : ' + error);
 	      });
+	  return false;
+	};
+
+	this.changePicture = function(event) {
+	  event.preventDefault();
+	  if (!navigator.camera) {
+	      alert("Camera API not supported", "Error");
+	      return;
+	  }
+	  var options =   {   quality: 50,
+	                      destinationType: Camera.DestinationType.DATA_URL,
+	                      sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+	                      encodingType: 0     // 0=JPG 1=PNG
+	                  };
+
+	  navigator.camera.getPicture(
+	      function(imgData) {
+	          $('.media-object', this.$el).attr('src', "data:image/jpeg;base64,"+imgData);
+	      },
+	      function() {
+	          alert('Error taking picture', 'Error');
+	      },
+	      options);
+
 	  return false;
 	};
 
